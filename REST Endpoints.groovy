e9a21d049a710edcc41d8c1dcd539e06d6341ed1
JasonBSteele <jasonbsteele@live.co.uk>
2015-05-12 20:01:27 +0100

    Formatting

--- REST Endpoints.groovy
+++ REST Endpoints.groovy
@@ -6,7 +6,7 @@
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *	  http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
@@ -14,57 +14,57 @@
  *
  */
 definition(
-    name: "REST Endpoints",
-    namespace: "JasonBSteele",
-    author: "Jason Steele",
-    description: "Provides REST Endpoints ",
-    category: "My Apps",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    oauth: true)
+	name: "REST Endpoints",
+	namespace: "JasonBSteele",
+	author: "Jason Steele",
+	description: "Provides REST Endpoints ",
+	category: "My Apps",
+	iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+	iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+	iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+	oauth: true)
 
 
 preferences {
 	section("Control These Things...") {
 		input "switches", "capability.switch", title: "Which Switches?", required:false, multiple: true
-        input "dimmers", "capability.switchLevel", title: "Which Dimmers?", required:false, multiple: true
-        input "locks", "capability.lock", title: "Which Locks?", required:false, multiple: true
-    }
-    section("Sense These Things...") {
-        input "temperatures", "capability.temperatureMeasurement", title: "Which Temperatures?", required:false, multiple: true
+		input "dimmers", "capability.switchLevel", title: "Which Dimmers?", required:false, multiple: true
+		input "locks", "capability.lock", title: "Which Locks?", required:false, multiple: true
+	}
+	section("Sense These Things...") {
+		input "temperatures", "capability.temperatureMeasurement", title: "Which Temperatures?", required:false, multiple: true
 	}
 }
 
 mappings {
 
 	path("/phrases") {
-    	action: [
-        	GET: "listPhrases"
-        ]
-    }
+		action: [
+			GET: "listPhrases"
+		]
+	}
 	path("/phrase/:helloHomePhrase") {
-    	action: [
-        	GET: "executePhrase"
-        ]
-    }
-    path("/mode/:newMode") {
-    	action: [
-        	GET: "updateMode"
-        ]
-    }
-    
+		action: [
+			GET: "executePhrase"
+		]
+	}
+	path("/mode/:newMode") {
+		action: [
+			GET: "updateMode"
+		]
+	}
+	
 	path("/mode") {
-    	action: [
-        	GET: "showMode"
-        ]
-    }
-    path("/mode/:newMode") {
-    	action: [
-        	GET: "updateMode"
-        ]
-    }
-    
+		action: [
+			GET: "showMode"
+		]
+	}
+	path("/mode/:newMode") {
+		action: [
+			GET: "updateMode"
+		]
+	}
+	
 	path("/switches") {
 		action: [
 			GET: "listSwitches"
@@ -80,8 +80,8 @@ mappings {
 			GET: "updateSwitch"
 		]
 	}
-    
-    path("/dimmers") {
+	
+	path("/dimmers") {
 		action: [
 			GET: "listDimmers"
 		]
@@ -96,7 +96,7 @@ mappings {
 			GET: "updateDimmer"
 		]
 	}
-    
+	
 	path("/locks") {
 		action: [
 			GET: "listLocks"
@@ -112,20 +112,18 @@ mappings {
 			GET: "updateLock"
 		]
 	}
-    
-    path("/temperatures") {
+	
+	path("/temperatures") {
 		action: [
 			GET: "listTemperatures"
 		]
 	}
-	path("/locks/:id") {
+	path("/temperatures/:id") {
 		action: [
 			GET: "showTemperature"
 		]
 	}
-    
 }
-
 
 def installed() {
 	log.debug "Installed with settings: ${settings}"
@@ -147,25 +145,26 @@ def initialize() {
 //phrase
 def listPhrases () {
 	def phrases = location.helloHome?.getPhrases()
-    phrases?.collect{'"phrase":"' + it.label +'"'}
+	phrases?.collect{'"phrase":"' + it.label +'"'}
 }
 
 void executePhrase() {
 	log.debug "executePhrase"
-    def phrase = params.helloHomePhrase
-    log.debug "Executing phrase ${phrase}"
+	def phrase = params.helloHomePhrase
+	log.debug "Executing phrase ${phrase}"
 	location.helloHome.execute(phrase)
-    }
+}
 
 
 //mode
 def showMode() {
 	[mode: location.mode]
-    }
+}
+
 void updateMode() {
 	def newMode = params.newMode
 	setLocationMode(newMode)
-    }
+}
 
 //switches
 def listSwitches() {
@@ -213,50 +212,48 @@ def showTemperature() {
 	queryDeviceToJson(temperatures, "temperature")
 }
 
-
 def deviceHandler(evt) {}
 
 private void updateToggle(devices) {
 	log.debug "updateToggle request: params: ${params}, devices: $devices.id"
-    
-    
+	
+	
 	//def command = request.JSON?.command
-    def command = params.command
-    //let's create a toggle option here
+	def command = params.command
+	//let's create a toggle option here
 	if (command) 
-    {
+	{
 		def device = devices.find { it.id == params.id }
 		if (!device) {
 			httpError(404, "Device not found")
-		} else {
-        	if(command == "toggle")
-       		{
-            	if(device.currentValue('switch') == "on")
-                  device.off();
-                else
-                  device.on();
-       		}
-       		else
-       		{
+		} 
+        else {
+			if(command == "toggle") {
+				if(device.currentValue('switch') == "on")
+				  device.off();
+				else
+				  device.on();
+	   		}
+	   		else {
 				device."$command"()
-            }
+			}
 		}
 	}
 }
 
 private void updateLevel(devices) {
 	log.debug "updateLevel request: params: ${params}, devices: $devices.id"
-    
-    //def level = params.level
-    Integer level = params.int('level')
+	
+	//def level = params.level
+	Integer level = params.int('level')
 
-	if (level) 
-    {
+	if (level) {
 		def device = devices.find { it.id == params.id }
 		if (!device) {
 			httpError(404, "Device not found")
-		} else {
-        	device.setLevel(level)
+		} 
+        else {
+			device.setLevel(level)
 		}
 	}
 }
@@ -264,12 +261,12 @@ private void updateLevel(devices) {
 
 private deviceToJson(device, attributeName) {
 	if (!device) 
-    	null
-        
+		null
+		
 	def s = device.currentState(attributeName)
 	def deviceMap = [id: device.id, name: device.displayName, unitTime: s?.date?.time]
-    deviceMap[attributeName] = s?.value
-    deviceMap
+	deviceMap[attributeName] = s?.value
+	deviceMap
 }
 
 private queryDeviceToJson(devices, attributeName) {
